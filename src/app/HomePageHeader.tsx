5dcb059f9f3b162db6f95f3835a99cc53bea2199
acyang97 <acyang97@gmail.com>
2023-03-13 00:58:30 +0800

    Add CTA

--- src/app/HomePageHeader.tsx
+++ src/app/HomePageHeader.tsx
@@ -30,6 +30,27 @@ const HomePageHeader = () => {
           <h1 className="title-font text-4xl mb-4 font-bold text-gray-900">
             I&#39;ve been to 28 countries.
           </h1>
+          {/* TODO Crrate the destination page */}
+          <Link
+            href="/"
+            role="button"
+            className="text-white bg-sky-600 hover:bg-sky-700 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-xl px-5 py-2.5 text-center inline-flex items-center"
+          >
+            Check Them Out
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5 ml-2 -mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </Link>
         </div>
       </div>
     </header>
