595df2ae4d7a34c1dd5124f47458f1618c0a4752
acyang97 <acyang97@gmail.com>
2023-03-20 17:57:48 +0800

    update footer

--- src/app/AppFooter.tsx
+++ src/app/AppFooter.tsx
@@ -18,6 +18,17 @@ const AppFooter = () => {
           />
           <span className="ml-3 text-xl">Chun&#39;s Odyssey</span>
         </a>
+        <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+          © 2023 -{" "}
+          <a
+            href="https://twitter.com/knyttneve"
+            className="text-gray-600 ml-1"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Chun&#39;s Odyssey
+          </a>
+        </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
           <a
             target="_blank"
