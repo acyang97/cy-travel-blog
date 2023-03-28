a683986b9738a35596727195d05225c3808d5afe
acyang97 <acyang97@gmail.com>
2023-03-28 11:54:22 +0800

    add croatia photos

--- src/constants/countryPreviewPhotosCompiled.ts
+++ src/constants/countryPreviewPhotosCompiled.ts
@@ -1,8 +1,10 @@
 import { CountryPreviewPhotos } from "@/interfaces/Country.interface";
+import { CroatiaPreviewPhotos } from "./countryPhotos/croatiaPhotos";
 import { HungaryPreviewPhotos } from "./countryPhotos/hungaryPhotos";
 import { UsaPreviewPhotos } from "./countryPhotos/usaPhotos";
 
 export const countryPreviewPhotos: Map<String, CountryPreviewPhotos> = new Map([
   ["USA", UsaPreviewPhotos],
   ["Hungary", HungaryPreviewPhotos],
+  ["Croatia", CroatiaPreviewPhotos],
 ]);
