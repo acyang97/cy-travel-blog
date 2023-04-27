7756436b89e4a70aa10b3e5adb542e04f2f41403
acyang97 <acyang97@gmail.com>
2023-04-28 00:37:13 +0800

    add japan prefectures data

---
+++ src/constants/Japan/japanPrefecturesData.ts
@@ -0,0 +1,237 @@
+export const JAPAN_PREFECTURES_DATA = [
+  {
+    prefecture: "Aichi",
+    region: "Chubu",
+    visited: true,
+  },
+  {
+    prefecture: "Akita",
+    region: "Tohoku",
+    visited: true,
+  },
+  {
+    prefecture: "Aomori",
+    region: "Tohoku",
+    visited: true,
+  },
+  {
+    prefecture: "Chiba",
+    region: "Kanto",
+    visited: false,
+  },
+  {
+    prefecture: "Ehime",
+    region: "Shikoku",
+    visited: false,
+  },
+  {
+    prefecture: "Fukui",
+    region: "Chubu",
+    visited: false,
+  },
+  {
+    prefecture: "Fukuoka",
+    region: "Kyushu",
+    visited: true,
+  },
+  {
+    prefecture: "Fukushima",
+    region: "Tohoku",
+    visited: true,
+  },
+  {
+    prefecture: "Gifu",
+    region: "Chubu",
+    visited: true,
+  },
+  {
+    prefecture: "Gunma",
+    region: "Kanto",
+    visited: false,
+  },
+  {
+    prefecture: "Hiroshima",
+    region: "Chugoku",
+    visited: true,
+  },
+  {
+    prefecture: "Hokkaido",
+    region: "Hokkaido",
+    visited: true,
+  },
+  {
+    prefecture: "Hyogo",
+    region: "Kansai",
+    visited: true,
+  },
+  {
+    prefecture: "Ibaraki",
+    region: "Kanto",
+    visited: true,
+  },
+  {
+    prefecture: "Iwate",
+    region: "Tohoku",
+    visited: true,
+  },
+  {
+    prefecture: "Ishikawa",
+    region: "Chubu",
+    visited: true,
+  },
+  {
+    prefecture: "Kagawa",
+    region: "Shikoku",
+    visited: false,
+  },
+  {
+    prefecture: "Kagoshima",
+    region: "Kyushu",
+    visited: true,
+  },
+  {
+    prefecture: "Kanagawa",
+    region: "Kanto",
+    visited: false,
+  },
+  {
+    prefecture: "Kochi",
+    region: "Shikoku",
+    visited: false,
+  },
+  {
+    prefecture: "Kumamoto",
+    region: "Kyushu",
+    visited: true,
+  },
+  {
+    prefecture: "Kyoto",
+    region: "Kansai",
+    visited: true,
+  },
+  {
+    prefecture: "Mie",
+    region: "Kansai",
+    visited: true,
+  },
+  {
+    prefecture: "Miyagi",
+    region: "Tohoku",
+    visited: true,
+  },
+  {
+    prefecture: "Miyazaki",
+    region: "Kysuhu",
+    visited: true,
+  },
+  {
+    prefecture: "Nagano",
+    region: "Chubu",
+    visited: true,
+  },
+  {
+    prefecture: "Nagasaki",
+    region: "Kyushu",
+    visited: true,
+  },
+  {
+    prefecture: "Nara",
+    region: "Kansai",
+    visited: true,
+  },
+  {
+    prefecture: "Niigata",
+    region: "Chubu",
+    visited: false,
+  },
+  {
+    prefecture: "Oita",
+    region: "Kyushu",
+    visited: true,
+  },
+  {
+    prefecture: "Okayama",
+    region: "Chugoku",
+    visited: false,
+  },
+  {
+    prefecture: "Okinawa",
+    region: "Okinawa",
+    visited: false,
+  },
+  {
+    prefecture: "Osaka",
+    region: "Kansai",
+    visited: true,
+  },
+  {
+    prefecture: "Saga",
+    region: "Kyushu",
+    visited: false,
+  },
+  {
+    prefecture: "Saitama",
+    region: "Kanto",
+    visited: false,
+  },
+  {
+    prefecture: "Shiga",
+    region: "Kansai",
+    visited: false,
+  },
+  {
+    prefecture: "Shimane",
+    region: "Chugoku",
+    visited: false,
+  },
+  {
+    prefecture: "Shizuoka",
+    region: "Chubu",
+    visited: false,
+  },
+  {
+    prefecture: "Tochigi",
+    region: "Kanto",
+    visited: false,
+  },
+  {
+    prefecture: "Tokushima",
+    region: "Shikoku",
+    visited: false,
+  },
+  {
+    prefecture: "Tokyo",
+    region: "Kanto",
+    visited: true,
+  },
+  {
+    prefecture: "Tottori",
+    region: "Chugoku",
+    visited: false,
+  },
+  {
+    prefecture: "Toyama",
+    region: "Chubu",
+    visited: true,
+  },
+  {
+    prefecture: "Wakayama",
+    region: "Kansai",
+    visited: true,
+  },
+  {
+    prefecture: "Yamagata",
+    region: "Tohoku",
+    visited: true,
+  },
+  {
+    prefecture: "Yamaguchi",
+    region: "Chugoku",
+    visited: false,
+  },
+  {
+    prefecture: "Yamanashi",
+    region: "Chubu",
+    visited: true,
+  },
+];
